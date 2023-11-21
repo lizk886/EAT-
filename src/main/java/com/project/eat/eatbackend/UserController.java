da5ab5e1e904b8b6f4d0cc3197a8b5f60db771b0
acwhelan <acwhelan@usc.edu>
2023-11-21 06:55:37 -0500

    index.html configurations in UserController

--- src/main/java/com/project/eat/eatbackend/UserController.java
+++ src/main/java/com/project/eat/eatbackend/UserController.java
@@ -1,9 +1,6 @@
 package com.project.eat.eatbackend;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.InvalidDataAccessResourceUsageException;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 //import org.springframework.http.HttpStatus;
 //import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
--- src/main/java/com/project/eat/eatbackend/WebController.java
+++ src/main/java/com/project/eat/eatbackend/WebController.java
@@ -6,13 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WebController {
 
-    @GetMapping("/welcome")
-    public String WelcomePage() {
-        // this needs to return the html welcome page
-        return "welcome"; 
-    }
-
-   @GetMapping("/login")
+     @GetMapping("/")
    public String Login()
     {
     // displays the existing user form 
